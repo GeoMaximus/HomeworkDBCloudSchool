c38d448f53946f6b93a632419452f81a52b53fa8
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-04-15 13:08:02 +0300

    commented some lines

--- Homework08/src/main/java/com/db/account/AccountService.java
+++ Homework08/src/main/java/com/db/account/AccountService.java
@@ -9,11 +9,11 @@ import java.util.List;
 
 @Service
 public class AccountService {
-    @Autowired
-    TransferMoneyService transferMoneyService;
-    public AccountService(@Autowired List<TransferMoneyService> transferMoneyServiceList, @Autowired @Qualifier("transferMoneyExternal") TransferMoneyService transferMoneyService) {
-        this.transferMoneyService = transferMoneyService;
-    }
+//    @Autowired
+//    TransferMoneyService transferMoneyService;
+//    public AccountService(@Autowired List<TransferMoneyService> transferMoneyServiceList, @Autowired @Qualifier("transferMoneyExternal") TransferMoneyService transferMoneyService) {
+//        this.transferMoneyService = transferMoneyService;
+//    }
     public String generateIBAN(Account account) {
         return "001" + account.getCurrency() + account.getId() / 4 + "123456789";
     }
