14a42d9e7a251aefe0aff5f8bae2d18e2b17fae6
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-04-26 20:50:55 +0300

    added UserSecurityConfig, now it builds
    Signed-off-by: Geo Maximus <pescarugeorgiana19@gmail.com>

--- Homework08/src/main/java/com/db/authentication/TokenCreator.java
+++ Homework08/src/main/java/com/db/authentication/TokenCreator.java
@@ -1,14 +1,10 @@
 package com.db.authentication;
 
 import io.jsonwebtoken.Jwts;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.auditing.CurrentDateTimeProvider;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TokenCreator {
-    @Autowired
-    CurrentDateTimeProvider timeProvider;
 
     public String generateToken(String email) {
         return Jwts.builder().setSubject(email).compact();
